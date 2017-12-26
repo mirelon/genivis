4b27af179f8a61da29626730e4d29093227272f0
Michal Kováč <kovac@essential-data.sk>
2017-12-26 16:42:58 +0100

    Redirect to auth page if token invalid

--- app/controllers/authorized_controller.rb
+++ app/controllers/authorized_controller.rb
@@ -1,5 +1,10 @@
 class AuthorizedController < ApplicationController
 
+  rescue_from OAuth2::Error do
+    cookies.delete :token
+    redirect_to '/auth/auth'
+  end
+
   before_action do
     puts "cookies['token']: #{cookies['token']}"
     redirect_to '/auth/auth' unless cookies['token']
